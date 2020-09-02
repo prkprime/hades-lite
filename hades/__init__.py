ccec49a58e25acc08141ec19f7e39c536a3ec32c
Pratik Gorade <pratikgorade0@gmail.com>
2020-09-02 19:00:17 +0530

    added base template (starter) and index template
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- hades/__init__.py
+++ hades/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #create the app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
