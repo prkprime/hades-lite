2fcb409979769fc3a4155d6709b89e9e0a3a40a0
Pratik Gorade <pratikgorade0@gmail.com>
2020-09-03 21:00:37 +0530

    changed root paths to /admin
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- hades/__init__.py
+++ hades/__init__.py
@@ -15,11 +15,11 @@ from .models.user import Users
 from .models.event import Events
 from .models.participant import Participants
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/admin/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -27,16 +27,16 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/admin/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.username.data == 'prkprime' and form.password.data == 'prkaly007':
-            return redirect(url_for('index'))
+            return redirect(url_for('admin'))
         else:
             flash('Login Unsuccessful. Please check username or password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-@app.route('/reset_password')
+@app.route('/admin/reset_password')
 def reset_password():
     return redirect(url_for('login'))
