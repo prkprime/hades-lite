2f2a9300d3344e7441c6d18f35f180b9e61e88e1
Pratik Gorade <pratikgorade0@gmail.com>
2020-11-24 08:57:02 +0530

    change debug.sh for heroku
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- debug.sh
+++ debug.sh
@@ -2,9 +2,9 @@
 
 # shellcheck disable=SC1091
 
-#[[ -d "venv" ]] || python3.8 -m venv ./venv
+[[ -d "venv" ]] || python3.8 -m venv ./venv
 source venv/bin/activate
-#pip install -U -r requirements.txt
+pip install -U -r requirements.txt
 cp example.env .env
-python3 -m hades
-#gunicorn hades:app -b 0.0.0.0:5500
+#python3 -m hades
+gunicorn hades:app -b 0.0.0.0:5500
